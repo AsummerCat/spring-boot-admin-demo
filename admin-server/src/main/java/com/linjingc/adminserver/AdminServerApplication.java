65700a8e331d0191208d9f0855dbe907d1e4083f
summerCat <583188551@qq.com>
2019-07-31 22:52:27 +0800

    添加配置文件

--- admin-server/src/main/java/com/linjingc/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/linjingc/adminserver/AdminServerApplication.java
@@ -1,9 +1,12 @@
 package com.linjingc.adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
