965b1a9f90f716c40f1548e732b650de98a1366e
summerCat <583188551@qq.com>
2019-08-01 22:55:31 +0800

    搭建注册中心版本 整合完毕

---
+++ admin-server-eureka/src/test/java/com/linjingc/adminservereureka/AdminServerEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linjingc.adminservereureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminServerEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
